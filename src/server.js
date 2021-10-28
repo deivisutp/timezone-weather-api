a29fe0b8c4754b410708453db5e6a3b916349aae
Deivis <deivisutp@gmail.com>
2021-10-28 18:34:06 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -8,8 +8,16 @@ const appRoutes = require('./routes');
 const app = express();
 
 app.use(cors({
+    origin: "*",
     exposedHeaders: "X-Total-Count"
 }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
